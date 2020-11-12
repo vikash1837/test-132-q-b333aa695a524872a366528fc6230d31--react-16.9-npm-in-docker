4833c9bb3e34fa26a0a33c505dff1a7048271a08
Vikash kumar <64901451+vikash1837@users.noreply.github.com>
2020-11-12 18:35:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(){
+  const[Result,setResult]=useState("");
+  const clear=()=>{
+    setResult("");
+  }
+  const deletefromend=()=>{
+    setResult(Result.slice(0,Result.length-1);
+  }
+  const click=(event)=>{
+      setResult(Result.concat(event.target.name));
+  }
+  const Answer=()=>{
+    try{
+      setResult(eval(Result).toString());
+    }catch(err){
+      setResult("Err");
+    }
   return (
     <div className="App">
+    <form>
+    <input type="text",value={Result}/>
+    </form>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +38,27 @@ function App() {
           Learn React
         </a>
       </header>
+    <div className="keys">
+    <button id="clear" onClick={clear}>AC</button>
+    <button id="delete" onClick={deletefromend}>DELETE</button>
+    <button id="Result" onClick={Answer}>=</Result>
+    <button name="/" onClick={click}>/</button>
+    <button name="7" onClick={click}>7</button>
+    <button name="8" onClick={click}>8</button>
+    <button name="9" onClick={click}>9</button>
+    <button name="*" onClick={click}>*</button>
+    <button name="4" onClick={click}>4</button>
+    <button name="5" onClick={click}>5</button>
+    <button name="6" onClick={click}>6</button>
+    <button name="/" onClick={click}>-</button>
+    <button name="1" onClick={click}>1</button>
+    <button name="2" onClick={click}>2</button>
+    <button name="3" onClick={click}>3</button>
+    <button name="+" onClick={click}>+</button>
+    <button name="%" onClick={click}>%</button>
+    <button name="0" onClick={click}>0</button>
+    <button name="." onClick={click}>.</button>
+    </div>
     </div>
   );
 }
